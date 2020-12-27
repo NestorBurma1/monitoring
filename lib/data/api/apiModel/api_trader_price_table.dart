73012f9cbea9359e642b8e0e51729c1fbe5da5e4
Oleksii Khoroshun <alexeyhoroshunvk@gmail.com>
2020-12-27 22:48:36 +0200

    Complete api_trader_price_table.dart. Add different apiKeys in api_google_sheet_key.dart to receive different products from monitoring

--- lib/data/api/apiModel/api_trader_price_table.dart
+++ lib/data/api/apiModel/api_trader_price_table.dart
@@ -7,32 +7,169 @@ class ApiTraderPriceTable {
   final List<Product> listProducts;
   final List<String> listComments;
 
-  ApiTraderPriceTable.fromApi(List<dynamic> map)
-      : date = DateTime.parse(map[0]['dateAndTraderName']['date']),
-        traderName = map[0]['dateAndTraderName']['traderName'],
-        listMonth = _getListStringsFromMap(map[2]['listMonth']),
-        listProducts = _getListProductFromMap(map[3]['listProducts']),
-        listComments = map[4]['listComments'].isNotEmpty
-            ? _getListStringsFromMap(map[4]['listComments'])
-            : null;
+  ApiTraderPriceTable.fromApi(Map<String, dynamic> map)
+      : date = DateTime.parse(map['data'][0]['dateAndTraderName']['date']),
+        traderName = map['data'][0]['dateAndTraderName']['traderName'],
+        listMonth = _getListStringsListMonth(map['data'][2]['listMonth']),
+        listProducts = _getListProductFromMap(map['data'][3]['listProducts']),
+        listComments = map['data'].length>4 ?
+            _getListStringsListComments(map['data'][4]['listComments']): null;
 }
 
-List<String> _getListStringsFromMap(List<dynamic> map) {
-  List<String> _listMonth;
+List<String> _getListStringsListMonth(List<dynamic> map) {
+  List<String> _listMonth = [];
   map.forEach((value) {
-    return _listMonth.add(value);
+    if (value != null) {
+      value.forEach((key, data) {
+        return _listMonth.add(data);
+      });
+    }
+    return _listMonth;
   });
   return _listMonth;
 }
 
 List<Product> _getListProductFromMap(List<dynamic> map) {
-  List<Product> _listProduct;
-  map.forEach((value) => _listProduct.add(value));
+  List<Product> _listProduct = [];
+  map.forEach((data) {
+    if (data != null) {
+      bool isSellPrice = _containWordSell(data['product']);
+      bool month3DoublePrice = false;
+      bool month4DoublePrice = false;
+      bool month5DoublePrice = false;
+      bool month6DoublePrice = false;
+      bool month7DoublePrice = false;
+      bool month8DoublePrice = false;
+      bool month9DoublePrice = false;
+      bool month10DoublePrice = false;
+      bool month11DoublePrice = false;
+      bool month12DoublePrice = false;
+      bool month13DoublePrice = false;
+      if (data['month3'] != null)
+        month3DoublePrice = _doublePriceCheck(data['month3']);
+      if (data['month4'] != null)
+        month4DoublePrice = _doublePriceCheck(data['month4']);
+      if (data['month5'] != null)
+        month5DoublePrice = _doublePriceCheck(data['month5']);
+      if (data['month6'] != null)
+        month6DoublePrice = _doublePriceCheck(data['month6']);
+      if (data['month7'] != null)
+        month7DoublePrice = _doublePriceCheck(data['month7']);
+      if (data['month8'] != null)
+        month8DoublePrice = _doublePriceCheck(data['month8']);
+      if (data['month9'] != null)
+        month9DoublePrice = _doublePriceCheck(data['month9']);
+      if (data['month10'] != null)
+        month10DoublePrice = _doublePriceCheck(data['month10']);
+      if (data['month11'] != null)
+        month11DoublePrice = _doublePriceCheck(data['month11']);
+      if (data['month12'] != null)
+        month12DoublePrice = _doublePriceCheck(data['month12']);
+      if (data['month13'] != null)
+        month13DoublePrice = _doublePriceCheck(data['month13']);
+      Product _product = Product(
+          product: data['product'],
+          basis: data['basis'],
+          currentBuyPrice: isSellPrice ? null : _splitOrNull(data['price']),
+          currentSellPrice: isSellPrice ? _splitOrNull(data['price']) : null,
+          month3BuyPrice: month3DoublePrice
+              ? data['month3'].split('/').last.split('(').first
+              : _splitOrNull(data['month3']),
+          month3SellPrice: month3DoublePrice
+          ? data['month3'].split('/').first.split('(').first
+          : null,
+          month4BuyPrice: month4DoublePrice
+              ? data['month4'].split('/').last.split('(').first
+              : _splitOrNull(data['month4']),
+          month4SellPrice: month4DoublePrice
+              ? data['month4'].split('/').first.split('(').first
+              : null,
+          month5BuyPrice: month5DoublePrice
+              ? data['month5'].split('/').last.split('(').first
+              : _splitOrNull(data['month5']),
+          month5SellPrice: month5DoublePrice
+              ? data['month5'].split('/').first.split('(').first
+              :  null,
+          month6BuyPrice: month6DoublePrice
+              ? data['month6'].split('/').last.split('(').first
+              : _splitOrNull(data['month6']),
+          month6SellPrice: month6DoublePrice
+              ? data['month6'].split('/').first.split('(').first
+              :  null,
+          month7BuyPrice: month7DoublePrice
+              ? data['month7'].split('/').last.split('(').first
+              : _splitOrNull(data['month7']),
+          month7SellPrice: month7DoublePrice
+              ? data['month7'].split('/').first.split('(').first
+              :  null,
+          month8BuyPrice: month8DoublePrice
+              ? data['month8'].split('/').last.split('(').first
+              : _splitOrNull(data['month8']),
+          month8SellPrice: month8DoublePrice
+              ? data['month8'].split('/').first.split('(').first
+              :  null,
+          month9BuyPrice: month9DoublePrice
+              ? data['month9'].split('/').last.split('(').first
+              : _splitOrNull(data['month9']),
+          month9SellPrice: month9DoublePrice
+              ? data['month9'].split('/').first.split('(').first
+              :  null,
+          month10BuyPrice: month10DoublePrice
+              ? data['month10'].split('/').last.split('(').first
+              : _splitOrNull(data['month10']),
+          month10SellPrice: month10DoublePrice
+              ? data['month10'].split('/').first.split('(').first
+              :  null,
+          month11BuyPrice: month11DoublePrice
+              ? data['month11'].split('/').last.split('(').first
+              : _splitOrNull(data['month11']),
+          month11SellPrice: month11DoublePrice
+              ? data['month11'].split('/').first.split('(').first
+              :  null,
+          month12BuyPrice: month12DoublePrice
+              ? data['month12'].split('/').last.split('(').first
+              : _splitOrNull(data['month12']),
+          month12SellPrice: month12DoublePrice
+              ? data['month12'].split('/').first.split('(').first
+              :  null,
+          month13BuyPrice: month13DoublePrice
+              ? data['month13'].split('/').last.split('(').first
+              : _splitOrNull(data['month13']),
+          month13SellPrice: month13DoublePrice
+              ? data['month13'].split('/').first.split('(').first
+              :  null
+      );
+      return _listProduct.add(_product);
+    }
+  });
   return _listProduct;
 }
 
-DateTime getDate(Map map) {
-  DateTime date;
-  print(map);
-  return date;
+bool _containWordSell(String string) {
+  String lastWord = string.split(' ').last.toLowerCase();
+  return lastWord == 'продажа';
+}
+
+bool _doublePriceCheck(String str) {
+  if(str != null)
+    return str.contains('/');
+  else return false;
+}
+
+String _splitOrNull(String str) {
+  if (str != null)
+    return str.split('(').first;
+  else
+    return null;
+}
+
+List<String> _getListStringsListComments(List<String> list) {
+  List<String> _listMonth = [];
+  if(list.isNotEmpty) {
+    list.forEach((value) {
+      if (value != null) return _listMonth.add(value);
+    });
+    return _listMonth;
+  }
+  else return null;
 }
--- lib/data/api/service/get_table_service.dart
+++ lib/data/api/service/get_table_service.dart
@@ -3,20 +3,20 @@ import 'package:monitoring/data/api/apiModel/api_trader_price_table.dart';
 import 'package:monitoring/data/api/service/api_google_sheet_key.dart';
 
 class GetTableService{
-  final String _apiKey=ApiGoogleSheetKey.apiKey;
   static const _based_url =
       'https://script.googleusercontent.com/macros/echo?user_content_key=';
 
-  Future<ApiTraderPriceTable> getTable() async {
-    final response = await Dio().get<dynamic>(_based_url+_apiKey);
+  Future<ApiTraderPriceTable> getTable(String apiKey) async {
+    final response = await Dio().get<dynamic>(_based_url+apiKey);
     ApiTraderPriceTable.fromApi(response.data);
     return ApiTraderPriceTable.fromApi(response.data);
   }
 }
-
+const String _soybeanOilApiKey=ApiGoogleSheetKey.soybeanOilApiKey;
+const String _sunflowerOil=ApiGoogleSheetKey.sunflowerOilApiKey;
 void main() async {
   ApiTraderPriceTable apiTraderPriceTable =
-  await GetTableService().getTable();
+  await GetTableService().getTable(_soybeanOilApiKey);
   print(apiTraderPriceTable.listProducts);
 
 }
