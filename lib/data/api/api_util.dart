3a9bf120dd6e55e3f402e6b4262da6d2e84ad1bc
Oleksii Khoroshun <alexeyhoroshunvk@gmail.com>
2021-01-09 13:54:31 +0200

    Add in data: 1. api_util.dart 2. trader_price_table_data_repository.dart in data/repository.

---
+++ lib/data/api/api_util.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:monitoring/data/api/mapper/trader_price_table_mapper.dart';
+import 'package:monitoring/data/api/service/get_table_service.dart';
+import 'package:monitoring/domain/model/trader_price_table.dart';
+
+class ApiUtil {
+  final GetTableService _getTableService;
+
+  ApiUtil(this._getTableService);
+
+  Future<TraderPriceTable> getTraderPriceTable(
+      {@required String requestProduct}) async {
+    final result = await _getTableService.getTable(requestProduct);
+    return TraderPriceTableMapper.fromAPi(apiTable: result);
+  }
+
+}
--- lib/data/api/mapper/trader_price_table_mapper.dart
+++ lib/data/api/mapper/trader_price_table_mapper.dart
@@ -2,7 +2,7 @@ import 'package:monitoring/data/api/apiModel/api_trader_price_table.dart';
 import 'package:monitoring/domain/model/trader_price_table.dart';
 
 class TraderPriceTableMapper{
-  static TraderPriceTable fromAPi(ApiTraderPriceTable apiTable){
+  static TraderPriceTable fromAPi({ApiTraderPriceTable apiTable}){
     return TraderPriceTable(
       date: DateTime.tryParse(apiTable.date.toString()),
       traderName: apiTable.traderName,
